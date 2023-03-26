470599435827262bbe7bb53c32b51a66bd5dd1a9
Leonardo <diogoleo738@outlook.com>
2023-03-25 21:40:59 -0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,36 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url)
-    .then(function (Response) {
-        console.log(Response)
-    })
-    .catch(function (error) {
-        console.error(error)
-    })
-    .finally(function () {
-        console.log('requisiçao concluida')
-    })
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
 
+            <div class="detail">
+                <ol class="types">
+                    <li class="type"> grass </li>
+                    <li class="type"> poison </li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+fetch(url)
+    .then((Response) => Response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemonToLi(pokemon))
+
+            document.getElementsById('pokemonsList')
+        }
+    })
+    .catch((error) => console.error(error))
 
